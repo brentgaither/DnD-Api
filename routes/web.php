43065016a74b4a1324d13544a39ab1091eb9014e
Brent Gaither <brent.w.gaither@gmail.com>
2018-02-17 10:59:14 -0700

    Add passport authorize

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,31 @@ Route::get('/passport', 'PassportController@index')->name('passport.index');
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+
+Route::get('/', function () {
+$query = http_build_query([
+     'client_id' => 7,
+     'redirect_uri' => 'http://localhost:4200/callback',
+     'response_type' => 'code',
+     'scope' => '',
+    ]);
+
+return redirect ('http://127.0.0.1:8000/oauth/authorize?'.$query);
+});
+
+Route::get('/callback', function (Request $request){
+$http= new GuzzleHttp\Client;
+dd($request);
+$response = $http->post('http://127.0.0.1:8000/oauth/token',[
+    'form_params' => [
+            'grant_type' => 'authorization_code',
+            'client_id' => 7 ,
+            'client_secret' => 'Nog2JOZSXmDXWh3oz6StcvM3eBV26Ziy1DCU09OC' ,
+            'redirect_uri' => 'http://localhost:4200/callback',
+            'code' => $request->code,
+        ],
+    ]);
+
+return json_decode((string) $response->getBody(), true);
+});
