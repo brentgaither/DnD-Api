46b479cea2ddf0605df546530cf455bc92cc6d49
Brent Gaither <brent.w.gaither@gmail.com>
2018-05-08 18:06:15 -0600

    Update some seeding for wallets and add a foreign key

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -15,7 +15,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
       $faker = Faker\Factory::create();
-      for ($i = 0; $i < 10; $i++) {
+      for ($i = 0; $i < 20; $i++) {
         $user = Item::create(array(
             'name' => $faker->word,
             'description' => $faker->sentence,
@@ -23,17 +23,18 @@ class DatabaseSeeder extends Seeder
         ));
       }
       $faker = Faker\Factory::create();
-      for ($i = 0; $i < 10; $i++) {
+      for ($i = 0; $i < 20; $i++) {
         $user = User::create(array(
             'name' => $faker->word,
             'email' => $faker->email,
+            'password'=> bcrypt('admin')
         ));
       }
       $faker = Faker\Factory::create();
       for ($i = 0; $i < 10; $i++) {
         $user = UserItem::create(array(
-            'user_id' => $faker->numberBetween($min = 1, $max = 10),
-            'item_id' => $faker->numberBetween($min = 1, $max = 10),
+            'user_id' => User::first()->id,
+            'item_id' => Item::first()->id,
             'quantity' => $faker->numberBetween($min = 1, $max = 10)
         ));
       }
